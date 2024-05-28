e29f84045d1c97d8c111a028599e8e732ad7d09b
Biplob Hossain <biplob17h@gmail.com>
2024-05-27 22:35:04 -0700

    update

--- src/work/Pages/Home/GlobalSection/GlobalSection.jsx
+++ src/work/Pages/Home/GlobalSection/GlobalSection.jsx
@@ -1,16 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { FaAngleRight } from "react-icons/fa";
 
 const GlobalSection = () => {
   return (
     <div className="text-white mt-44 min-h-screen w-screen backgroundImg1 overflow-hidden">
-      <div className="mx-[13%]">
-        <div className="bg-[#460073] w-1/2 h-[300px] border mt-[100vh]">
-          <div>
-            <h1>an ethical business leader</h1>
-          </div>
+      <div className="">
+        <div className="flex border mt-[100vh]">
+          <div className="bg-[#460073] w-1/2 h-[300px]"></div>
+          <div className="bg-[#e2062e] w-1/2 h-[300px]  mt-[390px]"></div>
         </div>
-        <div className="bg-[#e2062e] w-1/2 h-[300px] ml-[50%] border mt-[12%]"></div>
-        <div className="bg-[#0041f0] w-1/2 h-[300px] border mt-[12%] ml-[25%] mb-[100vh]"></div>
+        <div className="bg-[#0041f0] w-1/2 h-[300px]  "></div>
       </div>
     </div>
   );
