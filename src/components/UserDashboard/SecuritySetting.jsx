6803f0c755bb910f5f43748f70a033f277e19aaa
SM Sakin <saklinemostaksakin58@example.com>
2024-05-29 03:18:25 +0600

    dashboard work done

--- src/components/UserDashboard/SecuritySetting.jsx
+++ src/components/UserDashboard/SecuritySetting.jsx
@@ -11,32 +11,41 @@ const SecuritySetting = () => {
       };
   return (
     <div className="p-4">
-      <h2 className="text-3xl font-bold mb-4">Security Settings</h2>
-      <div className="mb-4">
-        <label className="block mb-2 md:text-xl">Old Password</label>
-        <input
-          type="password"
-          value={oldPassword}
-          onChange={(e) => setOldPassword(e.target.value)}
-          className="w-[500px] p-2 border border-gray-300 rounded"
-        />
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2 md:text-xl">New Password</label>
-        <input
-          type="password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          className="w-[500px] p-2 border border-gray-300 rounded"
-        />
-      </div>
-      <button onClick={handleChangePassword} className="px-4 py-2  text-white rounded">
-        Forget Password?
-      </button>
-      <button onClick={handleChangePassword} className="ml-[20px] px-4 py-2 bg-purple-700 text-white rounded">
-        Change Password
-      </button>
-    </div>
+  <h2 className="text-3xl font-bold mb-4">Security Settings</h2>
+  <div className="mb-4">
+    <label className="block mb-2 md:text-xl">Old Password</label>
+    <input
+      type="password"
+      value={oldPassword}
+      onChange={(e) => setOldPassword(e.target.value)}
+      className="w-full md:w-[500px] p-2 border border-gray-300 rounded"
+    />
+  </div>
+  <div className="mb-4">
+    <label className="block mb-2 md:text-xl">New Password</label>
+    <input
+      type="password"
+      value={newPassword}
+      onChange={(e) => setNewPassword(e.target.value)}
+      className="w-full md:w-[500px] p-2 border border-gray-300 rounded"
+    />
+  </div>
+  <div className="flex flex-col md:flex-row md:items-center">
+    <button
+      onClick={handleChangePassword}
+      className="px-4 py-2 mb-2 md:mb-0 md:mr-4 text-white bg-blue-500 rounded"
+    >
+      Forget Password?
+    </button>
+    <button
+      onClick={handleChangePassword}
+      className="px-4 py-2 bg-purple-700 text-white rounded"
+    >
+      Change Password
+    </button>
+  </div>
+</div>
+
   )
 }
 
